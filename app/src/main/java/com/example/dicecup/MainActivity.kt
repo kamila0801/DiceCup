fd5e3cbf18f258c224ab280ccbe1d496bd755918
kamila0801 <kami7686@easv365.dk>
2022-02-22 14:19:05 +0100

    dices img + array

--- app/src/main/java/com/example/dicecup/MainActivity.kt
+++ app/src/main/java/com/example/dicecup/MainActivity.kt
@@ -4,6 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val diceId = intArrayOf(0,
+        R.drawable.dice1,
+        R.drawable.dice2,
+        R.drawable.dice3,
+        R.drawable.dice4,
+        R.drawable.dice5,
+        R.drawable.dice6)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
