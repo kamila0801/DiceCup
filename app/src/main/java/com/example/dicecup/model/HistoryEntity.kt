9e57eb263f133360aefdffcca6b9e2695b4d1836
soma199 <70133057+Laszlo199@users.noreply.github.com>
2022-03-01 17:33:43 +0100

    toggleBtn to change to images

--- app/src/main/java/com/example/dicecup/model/HistoryEntity.kt
+++ app/src/main/java/com/example/dicecup/model/HistoryEntity.kt
@@ -1,10 +1,9 @@
 package com.example.dicecup.model
-
 import java.io.Serializable
 import java.time.LocalDateTime
 
 data class HistoryEntity(
     var id: Int,
     var timestamp: LocalDateTime,
-    var dices: String ) :
+    val rolls: IntArray) :
     Serializable {}
